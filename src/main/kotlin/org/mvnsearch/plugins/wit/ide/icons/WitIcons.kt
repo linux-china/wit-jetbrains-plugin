edfe591c233fd7a64e12280e7c42eae05e5437d7
Libing Chen <libing.chen@gmail.com>
2023-02-17 16:43:08 +0800

    app: WitIcons

---
+++ src/main/kotlin/org/mvnsearch/plugins/wit/ide/icons/WitIcons.kt
@@ -0,0 +1,7 @@
+package org.mvnsearch.plugins.wit.ide.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object WitIcons {
+    val WASM = IconLoader.findIcon("/icons/wasm.svg", WitIcons::class.java.classLoader)!!
+}
