1cbad4436e75ed70ba4d4d7da953c9740b01d046
Libing Chen <libing.chen@gmail.com>
2023-02-24 16:33:15 +0800

    app: WitBraceMatcher

---
+++ src/main/kotlin/org/mvnsearch/plugins/wit/lang/insight/WitBraceMatcher.kt
@@ -0,0 +1,23 @@
+package org.mvnsearch.plugins.wit.lang.insight
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import org.mvnsearch.plugins.wit.lang.psi.WitTypes
+
+
+class WitBraceMatcher : PairedBraceMatcher {
+    private val pairs = arrayOf(BracePair(WitTypes.LBRACE, WitTypes.RBRACE, true))
+    override fun getPairs(): Array<BracePair> {
+        return pairs
+    }
+
+    override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?): Boolean {
+        return true
+    }
+
+    override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int {
+        return openingBraceOffset
+    }
+}
