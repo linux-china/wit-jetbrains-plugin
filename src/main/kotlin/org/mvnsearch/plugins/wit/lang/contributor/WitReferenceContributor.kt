c3f12c662651c5b908dddd1378617dcddb0ff0a5
Libing Chen <libing.chen@gmail.com>
2023-02-24 16:10:15 +0800

    chore: polishing

--- src/main/kotlin/org/mvnsearch/plugins/wit/lang/contributor/WitReferenceContributor.kt
+++ src/main/kotlin/org/mvnsearch/plugins/wit/lang/contributor/WitReferenceContributor.kt
@@ -2,36 +2,26 @@ package org.mvnsearch.plugins.wit.lang.contributor
 
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.*
-import com.intellij.psi.util.parentOfType
 import com.intellij.util.ProcessingContext
-import org.mvnsearch.plugins.wit.lang.psi.WitUsePath
-import org.mvnsearch.plugins.wit.lang.psi.WitUsePathPart
+import org.mvnsearch.plugins.wit.lang.psi.WitUsePathPkgName
 
 
 class WitReferenceContributor : PsiReferenceContributor() {
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
         registrar.registerReferenceProvider(
-            PlatformPatterns.psiElement(),
+            PlatformPatterns.psiElement(WitUsePathPkgName::class.java),
             object : PsiReferenceProvider() {
                 override fun getReferencesByElement(
                     element: PsiElement,
                     context: ProcessingContext
                 ): Array<PsiReference> {
-                    if (element is WitUsePathPart) {
-                        val pathPart: WitUsePathPart = element as WitUsePathPart
-                        val witUsePath = pathPart.parentOfType<WitUsePath>()
-                        if (witUsePath != null && witUsePath.text.startsWith("pkg.")) {
-                            val pathText = witUsePath.text
-                            var pkgName = pathText.substring(4)
-                            if (pkgName.contains(".")) {
-                                pkgName = pkgName.substring(0, pkgName.lastIndexOf("."))
-                            }
-                            if (element.containingFile != null) {
-                                element.containingFile.parent?.let {
-                                    val pkgFile = it.findFile("$pkgName.wit")
-                                    if (pkgFile != null) {
-                                        return arrayOf(pkgFile.reference!!)
-                                    }
+                    if (element is WitUsePathPkgName) {
+                        val pkgName = element.text
+                        if (element.containingFile != null) {
+                            element.containingFile.parent?.let {
+                                val pkgFile = it.findFile("$pkgName.wit")
+                                if (pkgFile != null) {
+                                    return arrayOf(pkgFile.reference!!)
                                 }
                             }
                         }
