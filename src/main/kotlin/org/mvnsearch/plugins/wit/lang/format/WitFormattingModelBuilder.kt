3f64536158da2b32d5c3f61c108b441f8df20dc8
Libing Chen <libing.chen@gmail.com>
2023-02-25 17:18:27 +0800

    chore: polishing

--- src/main/kotlin/org/mvnsearch/plugins/wit/lang/format/WitFormattingModelBuilder.kt
+++ src/main/kotlin/org/mvnsearch/plugins/wit/lang/format/WitFormattingModelBuilder.kt
@@ -7,26 +7,27 @@ import org.mvnsearch.plugins.wit.lang.psi.WitTypes
 
 
 class WitFormattingModelBuilder : FormattingModelBuilder {
-    private fun createSpaceBuilder(settings: CodeStyleSettings): SpacingBuilder {
-        return SpacingBuilder(settings, WitLanguage)
-            .around(WitTypes.EQ).spaceIf(true)
-            .before(WitTypes.DEFAULT_KEYWORD).none()
-            .before(WitTypes.REFER_KEYWORD).spaces(2)
-    }
+    private fun SpacingBuilder.RuleBuilder.strictSpaces(needSpace: Boolean) =
+        spacing(needSpace.toInt(), needSpace.toInt(), 0, false, 0)
+
+    private fun Boolean.toInt() = if (this) 1 else 0
 
     override fun createModel(formattingContext: FormattingContext): FormattingModel {
-        val codeStyleSettings = formattingContext.codeStyleSettings
-        return FormattingModelProvider
-            .createFormattingModelForPsiFile(
-                formattingContext.containingFile,
-                WitBlock(
-                    formattingContext.node,
-                    Wrap.createWrap(WrapType.NONE, false),
-                    Alignment.createAlignment(),
-                    createSpaceBuilder(codeStyleSettings)
-                ),
-                codeStyleSettings
-            )
+        val settings = formattingContext.codeStyleSettings
+        val spacingBuilder = createSpacingBuilder(settings)
+        val root = WitBlock(
+            formattingContext.node, Wrap.createWrap(WrapType.NONE, false),
+            Alignment.createAlignment(), spacingBuilder
+        )
+        return FormattingModelProvider.createFormattingModelForPsiFile(formattingContext.containingFile, root, settings)
+    }
+
+
+    private fun createSpacingBuilder(settings: CodeStyleSettings): SpacingBuilder {
+        return SpacingBuilder(settings, WitLanguage)
+            .after(WitTypes.COLON).strictSpaces(true)
+            .before(WitTypes.ARROW).spaces(1)
+            .after(WitTypes.ARROW).strictSpaces(true)
     }
 
 }
