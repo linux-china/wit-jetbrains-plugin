8daf749524d9c918c259d75dd8e977077d06b4f7
Libing Chen <libing.chen@gmail.com>
2024-10-18 10:27:57 +0800

    chore: polishing

--- src/main/kotlin/org/mvnsearch/plugins/wit/lang/WitLanguage.kt
+++ src/main/kotlin/org/mvnsearch/plugins/wit/lang/WitLanguage.kt
@@ -3,5 +3,6 @@ package org.mvnsearch.plugins.wit.lang
 import com.intellij.lang.Language
 
 object WitLanguage : Language("WIT") {
+    private fun readResolve(): Any = WitLanguage
     override fun isCaseSensitive() = true
 }
